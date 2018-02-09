a9b5cf07dc159890523ebe42aeaf05fd3d18037e
kevin jiang <kevinjiang2009@yahoo.com>
2018-02-09 03:55:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,52 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(n) {
+  n++
+  return n
+}
+
+function dec(n) {
+  n--
+  return n
+}
+
+function makeInt(n) {
+  parseInt(n)
+  return n
+}
+
+function makeInt(n, 10) {
+  parseInt(n)
+  return n
+}
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
+
+preserveDecimal('2.2')
+
+function preserveDecimal(n) {
+  parseFloat(n)
+  return n
+}
+
+preserveDecimal("Hi")
+
+
+
